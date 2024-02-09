cbee668ec28ee8899c97bc02951c3f60176b7a2d
thiraphat.ch <thiraphat.ch@rmuti.ac.th>
2024-02-09 21:13:36 +0700

    Add login and register pages and routes. (generated by blackbox.ai )

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Login from './page/Login.jsx'
+import Register from './page/Register.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/Register",
+    element: <Register />
+  }
+
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
