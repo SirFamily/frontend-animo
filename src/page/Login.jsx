2733a78839f59dbfa10e12b6fcf37a4a488c3de0
thiraphat.ch <thiraphat.ch@rmuti.ac.th>
2024-02-10 10:53:09 +0700

    Add login form and register link to the login page.

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-import Nav from '../nav/Nav'
+import React from "react";
+import { Link } from "react-router-dom";
+import catImg from "../assets/image (14).png";
+import Nav from "../nav/Nav";
 
 function Home() {
   return (
-    <div>
-        <Nav />
-      <h1>Login</h1>
-    </div>
-  )
+    <>
+      <Nav />
+      <div className="flex flex-row justify-center">
+        <div className="flex flex-col justify-center gap-[20px] w-80 h-96 bg-gray-300 shadow-md ">
+          <div>Login</div>
+          <div>
+            <form action="">
+              <input type="text" id="username" placeholder="username" />
+              <br />
+              <input type="password" id="password" placeholder="password" />
+              <br />
+              <button type="submit">Submit</button>
+              <br />
+            </form>
+          </div>
+          <hr />
+          <div>
+            You don’t have an account ?<Link to="/register">Register</Link>{" "}
+          </div>
+        </div>
+        <div className="shadow-md">
+          <img src={catImg} alt="" className="w-80 h-96 bg-cover bg-center" />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
