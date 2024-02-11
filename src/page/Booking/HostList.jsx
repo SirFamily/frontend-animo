f9268472045db3a88107c91b2a4cfada17670508
thiraphat.ch <thiraphat.ch@rmuti.ac.th>
2024-02-11 17:15:06 +0700

    Add HostList page to user router (generated by blackbox.ai )

---
+++ src/page/Booking/HostList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HostList() {
+  return (
+    <div>
+        <h1 className="text-center text-3xl font-bold mt-8">Hosts</h1>
+      <table class="w-full table-fixed border-collapse shadow-md rounded my-6"></table>
+    </div>
+  )
+}
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Nav from "../nav/Nav";
+import useAuth from "../hooks/useAuth";
 import Home from "../page/Home";
 import Login from "../page/Login";
 import Register from "../page/Register";
-import useAuth from "../hooks/useAuth";
-import Nav from "../nav/Nav";
+
+import HostList from "../page/Booking/HostList";
+
+
 
 const guestRouter = createBrowserRouter([
   {
@@ -26,29 +30,21 @@ const guestRouter = createBrowserRouter([
   },
 ]);
 
-// const userRouter = createBrowserRouter([
-//     {
-//       path: "/",
-//       element: (
-//         <>
-//           <Nav />
-//           <Home />
-//           <Outlet />
-//         </>
-//       ),
-//     },
-//     {
-//       path: "/login",
-//       element: <Login />,
-//     },
-//     {
-//       path: "register",
-//       element: <Register />,
-//     },
-//   ]);
+const userRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Nav />
+          <HostList />
+          <Outlet />
+        </>
+      ),
+    },
+  ]);
 
 export default function AppRouter() {
   const { user } = useAuth();
-  // const finalRouter = user?.id ? userRouter : guestRouter
-  return <RouterProvider router={guestRouter} />;
+  const finalRouter = user?.id ? userRouter : guestRouter
+  return <RouterProvider router={finalRouter} />;
 }
