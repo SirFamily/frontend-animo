298377cfcf81affb3531748ca9442b9c3f07a862
thiraphat.ch <thiraphat.ch@rmuti.ac.th>
2024-02-18 01:28:22 +0700

    Add axios to fetch host data from the backend and display it on the frontend.

---
+++ src/page/Booking/HostDetailForBooking.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import ModelPopup from '../../component/ModelPopup'
+import axios from 'axios';
+
+export default function HostDetailForBooking({onClose,selectedHost}) {
+    const [roomsData, setRoomsData] = useState([]);
+
+    const hostId = selectedHost.id
+    useEffect(() => {
+        const getData = async () => {
+          try {
+            const token = localStorage.getItem("token");
+
+            const response = await axios.get(
+              `http://localhost:8112/p/user/host/room/p/${hostId}/room`,
+              { headers: { Authorization: `Bearer ${token}` } }
+            );
+            setRoomsData(response.data);
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+    
+        getData();
+      }, []); 
+    console.log(roomsData)
+  return (
+    <div>
+      <ModelPopup>
+        <h1 className="text-center">Host Detail</h1>
+        <hr />
+        <p>Name : {selectedHost.hostName}</p>
+        <img src="/assets/images/host_image.jpg" alt="" width={300} height={250} />
+        {roomsData.map((data) => (
+        <div
+          key={data.id}
+          onClick={() => togglePopupEdit(data)}
+        >
+          <div
+            style={{
+              background: `linear-gradient(0deg, rgba(0, 0, 0, 0.50) 0%, rgba(0, 0, 0, 0.00) 100%), url(${
+                data.rooms_img && data.rooms_img[0]
+                  ? data.rooms_img[0].urlImg
+                  : "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg"
+              }) lightgray 50% / cover no-repeat`,
+            }}
+          >
+            <div>{data.roomName}</div>
+          </div>
+        </div>
+      ))}
+        <button onClick={onClose}>Close</button>
+      </ModelPopup>
+    </div>
+  )
+}
--- src/page/Booking/HostList.jsx
+++ src/page/Booking/HostList.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import HostDetailForBooking from './HostDetailForBooking';
 
 export default function HostList() {
+  const [isData, setIsData] = useState([]);
+  const [isPopupOpen, setPopupOpen] = useState(false);
+  const [selectedHost, setSelectedHost] = useState(null);
+
+
+  const togglePopup = (data) => {
+    setSelectedHost(data);
+    setPopupOpen(!isPopupOpen);
+  };
+
+  useEffect(()=>{
+    const getData = async () => {
+      try{
+        const token = localStorage.getItem("token");
+        const response = await axios.get(
+          `http://localhost:8112/host`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+          
+        );
+        setIsData(response.data)
+      }catch(error){
+        
+      }
+    }
+    getData();
+  },[])
   return (
-    <div>
-        <h1 className="text-center text-3xl font-bold mt-8">Hosts</h1>
-      <table class="w-full table-fixed border-collapse shadow-md rounded my-6"></table>
+    <>
+        <h1 className="text-center text-3xl font-bold mt-8">Recommend</h1>
+        <div class="container mx-auto px-4">
+  {isData.map((host) => (
+    <div key={host.id} onClick={() => togglePopup(host)} class="bg-white p-4 rounded-lg shadow-md mt-4 mb-4">
+      <h2>{host.hostName}</h2>
+      <p class="text-gray-500 mb-2">{host.location}</p>
+      <p class="mb-2">{host.description}</p>
+      <p class="text-gray-700 font-bold">{host.propertyType}</p>
     </div>
+  ))}
+</div>
+{isPopupOpen && <HostDetailForBooking onClose={togglePopup} selectedHost={selectedHost}/>}
+    </>
   )
 }
